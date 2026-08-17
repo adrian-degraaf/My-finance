2d4714634b95df0bfc22967ca0e3953609b5b085
Adrian de Graaf <adrian@mac.home>
2026-08-17 15:21:06 +0100

    Add Supabase config and Vercel setup

---
+++ config.js
@@ -0,0 +1,4 @@
+window.FINANCE_CONFIG = {
+  supabaseUrl: 'https://wqootinlvlsijtlegtfx.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indxb290aW5sdmxzaWp0bGVndGZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4ODcxOTUsImV4cCI6MjEwMjQ2MzE5NX0.oEHZkK_RSkPo3eXWLvM-Xlylm6MMvNalTqwOdCnHsRs'
+};
